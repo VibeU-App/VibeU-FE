a6bc2fc24cc9bdc9243876598d77b3840095e70d
pillotop <polloitq@proton.me>
2026-06-18 12:06:52 +0700

    remove redundant files

--- lib/config/themes/app_shadow.dart
+++ lib/config/themes/app_shadow.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'app_colors.dart';
+
 class AppShadows {
   static BoxShadow low = BoxShadow(
-      color: Colors.black.withValues(alpha: 0.05),
+      color: AppColors.textPrimary500.withValues(alpha: 0.16),
       blurRadius: 4,
       offset: const Offset(0, 2),
   );
 
   static BoxShadow mid = BoxShadow(
-      color: Colors.black.withValues(alpha: 0.08),
+      color: AppColors.textPrimary500.withValues(alpha: 0.20),
       blurRadius: 8,
-      offset: const Offset(0, 4),
+      spreadRadius: -4,
+      offset: const Offset(0, 8),
   );
 
   static BoxShadow high = BoxShadow(
-      color: Colors.black.withValues(alpha: 0.12),
-      blurRadius: 16,
-      offset: const Offset(0, 8),
+      color: AppColors.textPrimary500.withValues(alpha: 0.24),
+      blurRadius: 32,
+      spreadRadius: -8,
+      offset: const Offset(0, 20),
   );
 }
