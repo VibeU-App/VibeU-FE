a673a55fe1b96ab615dee2b76d1055a653e23895
pillotop <polloitq@proton.me>
2026-06-22 20:55:39 +0700

    fix: replace classes Radius, Spacing with AppSizes Radius class coincides with flutter's Radius. Also made pubspec.yaml so that it enforces dart sdk >=v3.27.0

--- lib/config/themes/app_sizes.dart
+++ lib/config/themes/app_sizes.dart
@@ -1,5 +1,5 @@
+class AppSizes {
 /// Chứa các hằng số về khoảng cách (Padding, Margin, SizedBox)
-class Spacing {
   static const double s4 = 4.0;
   static const double s8 = 8.0;
   static const double s16 = 16.0;
@@ -7,10 +7,8 @@ class Spacing {
   static const double s32 = 32.0;
   static const double s48 = 48.0;
   static const double s64 = 64.0;
-}
 
 ///Chứa các hằng số về độ bo góc (Border Radius)
-class Radius {
   static const double r4 = 4.0; 
   static const double r8 = 8.0;
   static const double r12 = 12.0;
